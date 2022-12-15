487125c99b5245b347b9bf64404b2c9f2db571ea
dobuzi <dobuji@kakao.com>
2022-12-15 16:19:19 +0900

    like button basic function

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -13,7 +13,7 @@ bp = Blueprint('blog', __name__)
 def index():
 	db = get_db()
 	posts = db.execute(
-		'SELECT p.id, title, body, created, author_id, username'
+		'SELECT p.id, title, body, created, author_id, like_count, username'
 		' FROM post p JOIN user u ON p.author_id = u.id'
 		' ORDER BY created DESC'
 	).fetchall()
@@ -35,9 +35,9 @@ def create():
 		else:
 			db = get_db()
 			db.execute(
-				'INSERT INTO post (title, body, author_id)'
-				' VALUES (?, ?, ?)',
-				(title, body, g.user['id'])
+				'INSERT INTO post (title, body, author_id, like_count)'
+				' VALUES (?, ?, ?, ?)',
+				(title, body, g.user['id'], 0)
 			)
 			db.commit()
 			return redirect(url_for('blog.index'))
@@ -46,7 +46,7 @@ def create():
 
 def get_post(id, check_author=True):
 	post = get_db().execute(
-		'SELECT p.id, title, body, created, author_id, username'
+		'SELECT p.id, title, body, created, author_id, username, like_count'
 		' FROM post p JOIN user u ON p.author_id = u.id'
 		' WHERE p.id = ?',
 		(id, )
@@ -94,6 +94,26 @@ def delete(id):
 	db = get_db()
 	db.execute('DELETE FROM post WHERE id = ?', (id,))
 	db.commit()
+	return redirect(url_for('blog.index'))
+
+def like_or_dislike(is_like):
+	return 1 if is_like else -1
+
+@bp.route('/<int:id>/like', methods=('GET', 'POST'))
+@login_required
+def update_like_count(id, is_like=True):
+	post = get_post(id)
+	
+	if request.method == 'POST':
+		like_count = post['like_count'] + like_or_dislike(is_like)
+		db = get_db()
+		like = db.execute(
+			'UPDATE post SET like_count = ?'
+			' WHERE id = ?',
+			(like_count, id)
+		)
+		db.commit()
+	
 	return redirect(url_for('blog.index'))
 
 @bp.route('/<int:id>/detail')
