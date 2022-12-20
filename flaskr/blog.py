30f985410dc19643bef419766f9c06aed096d8e7
dobuzi <dobuji@kakao.com>
2022-12-20 15:37:29 +0900

    like button basic function; be robust

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -13,7 +13,7 @@ bp = Blueprint('blog', __name__)
 def index():
 	db = get_db()
 	posts = db.execute(
-		'SELECT p.id, title, body, created, author_id, like_count, username'
+		'SELECT p.id, title, body, created, author_id, username, likes'
 		' FROM post p JOIN user u ON p.author_id = u.id'
 		' ORDER BY created DESC'
 	).fetchall()
@@ -35,9 +35,9 @@ def create():
 		else:
 			db = get_db()
 			db.execute(
-				'INSERT INTO post (title, body, author_id, like_count)'
-				' VALUES (?, ?, ?, ?)',
-				(title, body, g.user['id'], 0)
+				'INSERT INTO post (title, body, author_id)'
+				' VALUES (?, ?, ?)',
+				(title, body, g.user['id'])
 			)
 			db.commit()
 			return redirect(url_for('blog.index'))
@@ -46,7 +46,7 @@ def create():
 
 def get_post(id, check_author=True):
 	post = get_db().execute(
-		'SELECT p.id, title, body, created, author_id, username, like_count'
+		'SELECT p.id, title, body, created, author_id, username, likes'
 		' FROM post p JOIN user u ON p.author_id = u.id'
 		' WHERE p.id = ?',
 		(id, )
@@ -96,25 +96,40 @@ def delete(id):
 	db.commit()
 	return redirect(url_for('blog.index'))
 
-def like_or_dislike(is_like):
-	return 1 if is_like else -1
-
 @bp.route('/<int:id>/like', methods=('GET', 'POST'))
 @login_required
-def update_like_count(id, is_like=True):
+def like(id):
 	post = get_post(id)
 	
 	if request.method == 'POST':
-		like_count = post['like_count'] + like_or_dislike(is_like)
 		db = get_db()
-		like = db.execute(
-			'UPDATE post SET like_count = ?'
-			' WHERE id = ?',
-			(like_count, id)
-		)
-		db.commit()
-	
+		click_like(db, post)
+		update_likes(db, id)
 	return redirect(url_for('blog.index'))
+
+def click_like(db, post):
+	db.execute(
+		'INSERT INTO like (user_id, post_id)'
+		' VALUES (?, ?)',
+		(g.user['id'], post['id'])
+	)
+	db.commit()
+	return
+
+def count_likes(db, id):
+	c = db.execute(
+		'SELECT COUNT(*) as cnt FROM like WHERE post_id = ?',
+		(id,)
+	).fetchone()
+	return c['cnt']
+
+def update_likes(db, id):
+	db.execute(
+		'UPDATE post SET likes = ? WHERE id = ?',
+		(count_likes(db, id), id)
+	)
+	db.commit()
+	return
 
 @bp.route('/<int:id>/detail')
 def detail(id):
